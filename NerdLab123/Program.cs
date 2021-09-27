e48da18bd1a5b4b177330bc0d305a50650cf8d79
EmmaroseKrusely <ekrusely@gmail.com>
2021-09-27 10:01:18 -0400

    done

--- NerdLab123/Program.cs
+++ NerdLab123/Program.cs
@@ -6,7 +6,43 @@ namespace NerdLab123
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+            Player();
+            
+         Console.ReadKey();
         }
+
+        static public void Player()
+        {
+            
+            Console.WriteLine("Enter your name: ");
+            string playerName = Console.ReadLine();
+            
+            Console.WriteLine($"Hello {playerName}");
+
+            Console.WriteLine("Enter your age: ");
+            int playerAge = Convert.ToInt32(Console.ReadLine());
+            if (playerAge >= 25)
+            {
+                Console.WriteLine($"{playerAge}....? WOW!! you're old.... Welcome I guess");
+            }
+            else
+            {
+                Console.WriteLine("Welcome young traveler");
+            }
+
+            Random luckGen = new Random();
+            decimal luck = luckGen.Next(1, 2);
+            
+            Random rollMethod = new Random();
+            decimal roll = rollMethod.Next(1, 10);
+
+            decimal luck1 = (roll * luck);
+
+            Console.WriteLine($"Your luck is {luck1}");
+
+        }
+
+
     }
 }
